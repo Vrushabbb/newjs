c3fada7fe048e518f7f6c8cf0b916ebcb42c4831
Vrushabbb <vrushabhbaravkar2005@gmail.com>
2024-12-21 16:01:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let num = prompt("enter a your marks");
-if(num<100 && num>80){
-    console.log("A");
-}
-else if(num>50 && num<80){
-    console.log("B");
-}
-else{
-    alert("You are fail bro");
-}
+let num = 69;
+alert('Hint : It is a Sex Position');
+while(true){
+    let j = prompt("Enter Number");
+    if(j==69){
+        alert("Congratulations");
+        break;
+    }
+    alert("Wrong Try Again")
 
+}
