6d4fe877ecf7a433bc4753c57ee9b1595f22ed68
Vrushabbb <vrushabhbaravkar2005@gmail.com>
2024-12-21 14:49:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let num = prompt("enter a your marks");
+if(num<100 && num>80){
+    console.log("A");
+}
+else if(num>50 && num<80){
+    console.log("B");
+}
+else{
+    alert("You are fail bro");
+}
+
